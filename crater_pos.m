70c799f8788a6c6eec290ec9072b34d8658d718c
alec-e-delgado <adelgado3240@sdsu.edu>
2026-02-12 02:41:07 -0800

    Update

--- crater_pos.m
+++ crater_pos.m
@@ -1,4 +1,4 @@
-function [WLS_e, LS_e, r_crater_I_nom, r_crater_aux_nom] = crater_pos(r_sc_I, sc_bearing, std_expanded, mean_expanded)
+function [WLS_e, LS_e, r_crater_I_nom, r_crater_aux_nom] = crater_pos(r_sc_I, sc_bearing, std_expanded, mean_expanded, leave)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %CRATER_POS
 %  - This script calculaes the positions of detected craters defined in
@@ -147,6 +147,15 @@ elevation = atan2(r_Ecrater_LOS(3,:), sqrt(r_Ecrater_LOS(1,:).^2+r_Ecrater_LOS(2
 std_elevation_analytical = std_expanded/sqrt((4-pi)/2);
 std_azimuth_analytical = std_expanded./sqrt((4-pi)/2)./cos(elevation_nom');
 
+if leave == 1
+    WLS_e = azimuth;
+    LS_e = elevation;
+    r_crater_I_no = 0;
+    r_crater_aux_nom = 0;
+    return 
+end
+
+
 %% Least Squares (LS) method
 
 L = length(r_crater_LOS_nom); % number of measurements 
@@ -226,38 +235,39 @@ WLS_e = norm(g_WLS_LOS);
 
 % fprintf("WLS Distance error: %.4f km \n", norm(g_WLS_LOS)/1000)
 
+
 %% Bias-Compensated Weighted Least Squares (BCWLS)
 
 % Pre allocate m matrix
-m_matrix = zeros(3,L);
-
-for i = 1:L
-
-    % Extract Wn and elements
-    W_n = W_block(2*i-1:2*i, 2*i-1:2*i);
-    a_n = W_n(1,1);
-    b_n = W_n(1,2);
-    c_n = W_n(2,2);
-
-    % Form a vectors
-    a_1n = [cos(azimuth(i));sin(azimuth(i));0];
-    a_2n = [sin(azimuth(i))*sin(elevation(i));-cos(azimuth(i))*sin(elevation(i));0];
-    a_3n = -[cos(azimuth(i))*cos(elevation(i));sin(azimuth(i))*cos(elevation(i));sin(elevation(i))];
-
-    % Form g vectors
-    g_1n = -do(i)*cos(elevation(i))*std_azimuth_analytical(i)^2*(a_n*a_1n+b_n*a_2n);
-    g_2n = c_n*do(i)*a_3n*std_elevation_analytical(i)^2;
-
-    % Calc m and save
-    m_n = g_1n+g_2n;
-    m_matrix(:,i) = m_n;
-end
-
-Atwn = sum(m_matrix,2);
-
-gamma_gWLS = inv(A'*W_block*A)*Atwn;
-
-g_BCWLS_LOS = g_WLS_LOS-gamma_gWLS;
+% m_matrix = zeros(3,L);
+% 
+% for i = 1:L
+% 
+%     % Extract Wn and elements
+%     W_n = W_block(2*i-1:2*i, 2*i-1:2*i);
+%     a_n = W_n(1,1);
+%     b_n = W_n(1,2);
+%     c_n = W_n(2,2);
+% 
+%     % Form a vectors
+%     a_1n = [cos(azimuth(i));sin(azimuth(i));0];
+%     a_2n = [sin(azimuth(i))*sin(elevation(i));-cos(azimuth(i))*sin(elevation(i));0];
+%     a_3n = -[cos(azimuth(i))*cos(elevation(i));sin(azimuth(i))*cos(elevation(i));sin(elevation(i))];
+% 
+%     % Form g vectors
+%     g_1n = -do(i)*cos(elevation(i))*std_azimuth_analytical(i)^2*(a_n*a_1n+b_n*a_2n);
+%     g_2n = c_n*do(i)*a_3n*std_elevation_analytical(i)^2;
+% 
+%     % Calc m and save
+%     m_n = g_1n+g_2n;
+%     m_matrix(:,i) = m_n;
+% end
+% 
+% Atwn = sum(m_matrix,2);
+% 
+% gamma_gWLS = inv(A'*W_block*A)*Atwn;
+% 
+% g_BCWLS_LOS = g_WLS_LOS-gamma_gWLS;
 
 % fprintf("BCWLS Distance error: %.4f km \n", norm(g_BCWLS_LOS)/1000)
 
--- least_squares_main.m
+++ least_squares_main.m
@@ -18,7 +18,8 @@ r_sc_I = [0.4811; 0.2580; 0.8379]*(distance_sc); % m
 
 % Max index value
 rep_matrix_size = size(repeat_matrix_detections);
-fprintf('Max index: %d\n', rep_matrix_size(1));
+num_craters = rep_matrix_size(1);
+fprintf('Max index: %d\n', num_craters);
 is_pass = false; % used to ensure index is chosen within the size of the array
 
 % Display list of craters
@@ -26,7 +27,7 @@ is_pass = false; % used to ensure index is chosen within the size of the array
 %     fprintf('%d -- sc_inc = %.2f -- crater_radius = %.2f\n', j, repeat_matrix_detections(j,3),repeat_matrix_detections(j,4))
 % end
 
-desired_indices = 1:length(x_2); % list of indices to use for crater generation
+desired_indices = 1:num_craters; % list of indices to use for crater generation
 
 % Extract crater quantities associated with desired indeces
 std_devs = repeat_matrix_detections(desired_indices,2); % rad
@@ -35,12 +36,10 @@ sc_bearings = repeat_matrix_detections(desired_indices, 3); % rad
 
 % Number of times to run simulation
 
-N = 10000;
+% N = 10000; % to use for Monte Carlo
+N = num_craters;
 
 % Pre allocate space for errors
-LS_errors = zeros(N, 1);
-WLS_errors = zeros(N, 1);
-
 mean_LS = zeros(N, 1);
 std_LS = zeros(N, 1);
 
@@ -49,19 +48,40 @@ std_WLS = zeros(N, 1);
 
 % for j = 1:jend
 
-for i = 1:N
-% Pass statistic values 
-    [WLS_e, LS_e, r_crater_I, r_crater_aux] = crater_pos(r_sc_I, sc_bearings, std_devs, means);
+leave = 1;
 
-    LS_errors(i) = LS_e;
-    WLS_errors(i) = WLS_e;
+[azimuth, elevation, r_crater_I, r_crater_aux] = crater_pos(r_sc_I, sc_bearings, std_devs, means,leave);
+[azimuth_sort, idx_sort] = sort(azimuth,'ascend');
+sc_bearings = sc_bearings(idx_sort);
+std_devs = std_devs(idx_sort);
+means = means(idx_sort);
 
-    mean_LS(i) = mean(LS_errors(1:i));
-    std_LS(i) = std(LS_errors(1:i));
+% Need to put above implementation in crater_pos, organize and just
+% extracxt i number of craters
 
-    mean_WLS(i) = mean(WLS_errors(1:i));
-    std_WLS(i) = std(WLS_errors(1:i));
+% The azimuth and elevation angles are not the same for each call of the
+% function, thats why it needs to be added within the function, just add it
+% there, sort, and extract the best i craters, use i as input to function
+
+
+leave = 0;
+% for i = 1:N % Monte Carlo
+for i = 3:num_craters % need minimum three measurements
+    for j = 1:10000 % num iterations
+    % Pass statistic values 
+    [WLS_e, LS_e, r_crater_I, r_crater_aux] = crater_pos(r_sc_I, sc_bearings(1:i), std_devs(1:i), means(1:i),leave);
+    
+        LS_errors(j) = LS_e;
+        WLS_errors(j) = WLS_e;
+    
+    end
+        mean_LS(i) = mean(LS_errors);
+        std_LS(i) = std(LS_errors);
+    
+        mean_WLS(i) = mean(WLS_errors);
+        std_WLS(i) = std(WLS_errors);
 end
+
 
 % % Scale errors by altitude
 % scaled_LS(:,j) = LS_errors(:,j)/altitude_sc * 100; 
@@ -107,21 +127,41 @@ end
 % 
 % legend('LS', 'WLS')
 
+% Monte Carlo
+% figure
+% hold on
+% plot(1:N, mean_LS,'LineWidth', 1.5)
+% plot(1:N,mean_WLS,'LineWidth', 1.5)
+% title('Mean convergence ')
+% xlabel('Runs')
+% ylabel('Mean (Absolute error)')
+% legend('LS', 'WLS')
+% 
+% figure
+% hold on
+% plot(1:N,std_LS ,'LineWidth', 1.5)
+% plot(1:N,std_WLS,'LineWidth', 1.5)
+% title('std convergence ')
+% xlabel('Runs')
+% ylabel('STD (absolute error)')
+% legend('LS', 'WLS')
+
+% Sorted Craters
 figure
 hold on
-plot(1:N, mean_LS,'LineWidth', 1.5)
-plot(1:N,mean_WLS,'LineWidth', 1.5)
-title('Mean convergence ')
-xlabel('Runs')
+plot(3:N, mean_LS(3:end),'LineWidth', 1.5)
+plot(3:N,mean_WLS(3:end),'LineWidth', 1.5)
+title('Mean')
+xlabel('Num Craters')
 ylabel('Mean (Absolute error)')
 legend('LS', 'WLS')
 
 figure
 hold on
-plot(1:N,std_LS ,'LineWidth', 1.5)
-plot(1:N,std_WLS,'LineWidth', 1.5)
-title('std convergence ')
-xlabel('Runs')
+plot(3:N,std_LS(3:end) ,'LineWidth', 1.5)
+plot(3:N,std_WLS(3:end),'LineWidth', 1.5)
+title('std')
+xlabel('Num Craters')
 ylabel('STD (absolute error)')
 legend('LS', 'WLS')
 
